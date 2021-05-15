2c9ef79a30fff997b20a9cae2325cbe469556d70
Vladislav Cheremisov <vladcheremisov@MacBook-Air-Vladislav.local>
2021-05-15 23:41:45 +0700

    contact list vc update

--- ContactsApp/Contact.swift
+++ ContactsApp/Contact.swift
@@ -5,4 +5,11 @@
 //  Created by Vladislav Cheremisov on 15.05.2021.
 //
 
-import Foundation
+struct Contact: Codable {
+    let name: String
+    let surname: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
--- ContactsApp/ContactListViewController.swift
+++ ContactsApp/ContactListViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ContactListViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
-
-
+    @IBOutlet var tableView: UITableView!
+    
+    private var contacts: [Contact] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
+
+// MARK: - UITAbleViewDataSource
+extension ContactListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Contact", for: indexPath)
+        let contact = contacts[indexPath.row]
+        cell.textLabel?.text = contact.fullName
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ContactListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+}
+
 
